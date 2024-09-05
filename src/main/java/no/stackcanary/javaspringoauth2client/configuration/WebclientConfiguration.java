e35618906e11217c4d45e72f0e359cd6734fbd81
Eirik Tveteraas <eirik.tveteraas@gmail.com>
2024-09-05 21:46:18 +0200

    some refactoring

--- src/main/java/no/stackcanary/javaspringoauth2client/configuration/WebclientConfiguration.java
+++ src/main/java/no/stackcanary/javaspringoauth2client/configuration/WebclientConfiguration.java
@@ -1,11 +1,11 @@
 package no.stackcanary.javaspringoauth2client.configuration;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -20,28 +20,24 @@ import java.util.List;
 @Configuration
 public class WebclientConfiguration {
 
-    @Bean(name = "clientRegistrations")
+    @Primary
+    @Bean
     public ReactiveClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties oAuth2ClientProperties) {
         List<ClientRegistration> registrations = new ArrayList<>(
                 new OAuth2ClientPropertiesMapper(oAuth2ClientProperties).asClientRegistrations().values());
         return new InMemoryReactiveClientRegistrationRepository(registrations);
     }
 
-    private WebClient.Builder getWebClientBuilder(ReactiveClientRegistrationRepository clientRegistrations) {
+    @Bean
+    public WebClient webclient(@Value("resource-server-url") String url, ReactiveClientRegistrationRepository clientRegistrations) {
         return WebClient.builder()
                 .codecs(configurer -> configurer
                         .defaultCodecs()
                         .maxInMemorySize(24 * 1024 * 1024))
                 .filter(new ServerOAuth2AuthorizedClientExchangeFilterFunction(
                         new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrations,
-                                new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrations))));
-    }
-
-    @Bean
-    public WebClient webclient(@Value("resource-server-url") String url, @Qualifier("clientRegistrations") ReactiveClientRegistrationRepository clientRegistrations) {
-        return getWebClientBuilder(clientRegistrations)
+                                new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrations))))
                 .baseUrl(url)
                 .build();
     }
-
 }
